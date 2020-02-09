36ef93faddd058a0dedb02e7e210b30ca15c995a
Masoud <masoudshiroie@gmail.com>
2020-02-09 20:21:14 +0100

    rand.py is updated to be more realistic

--- runTest/rand.py
+++ runTest/rand.py
@@ -17,22 +17,28 @@ def parsArgument():
 def calc_runtime(args):
     """Calculte runtime for a patch."""
     bench_mark_size = args.size # lines
-    max_number_of_errors = int(bench_mark_size / 1000)
+    print("****************************************************************************************")
+    print("")
+    ref_size = 200
+    max_number_of_errors = int(bench_mark_size / ref_size) + 1
     print(f"max num of errors {max_number_of_errors}")
     num_real_fixing_candidates = int( 7 * max_number_of_errors / 10 ) + 1
-    number_error_candidare_lines = int(2 * bench_mark_size / 1000) # number of error candidate lines 
+    print(f"num_real_fixing_candidates = {num_real_fixing_candidates}")
+    number_error_candidare_lines = int(0.2 * bench_mark_size / 100) + 1 # number of error candidate lines 
+    print(f"number_error_candidare_lines = {number_error_candidare_lines}")
     max_number_of_fixing_type = 4
     if num_real_fixing_candidates > max_number_of_errors :
         print("Error: num_real_fixing_candidates can not be bigger than max_number_of_errors")
         sys.exit(-1)
     number_of_virtual_fixing_candidates = number_error_candidare_lines * max_number_of_fixing_type
+    print(f"number_of_virtual_fixing_candidates = {number_of_virtual_fixing_candidates}")
     real_fix_set = set(range(num_real_fixing_candidates))
     line_nums = list(range(number_of_virtual_fixing_candidates))
     random.shuffle(line_nums)
     print(f"line_nums = {line_nums}")
     # comb_lines = list(combinations(line_nums, max_number_of_errors))
     # random.shuffle(comb_lines)
-    print("real fix set", real_fix_set)
+    print(f"real fix set = {real_fix_set}")
     # print("size possible fixes", len(comb_lines))
     cnt = 1
     one_exe_time = args.sim_time
@@ -53,10 +59,11 @@ def calc_runtime(args):
 if __name__ == "__main__":
     args = parsArgument()
     avg_time = 0
-    num_run = 100
+    num_run = 500
     for i in range(num_run):
         avg_time += calc_runtime(args)
-    print(avg_time/num_run)
+        if i > 0:
+            print(str(timedelta(seconds = (avg_time/i))))
     print("avg_run_time = ", str(timedelta(seconds = (avg_time/num_run))), "sec")
     
 
